343d144ef7e93b62204557127a497bf32233118b
SlaViK2006 <slava.ignatovich@gmail.com>
2025-03-17 20:47:09 +0100

    Complete Task2

--- CMake/cmake-3.25.3-tutorial-source/Step2/tutorial.cxx
+++ CMake/cmake-3.25.3-tutorial-source/Step2/tutorial.cxx
@@ -6,10 +6,10 @@
 #include "TutorialConfig.h"
 
 // TODO 11: Only include MathFunctions if USE_MYMATH is defined
-
+#ifdef USE_MYMATH
 // TODO 5: Include MathFunctions.h
 #include "MathFunctions.h"
-
+#endif
 int main(int argc, char* argv[])
 {
   if (argc < 2) {
@@ -24,7 +24,11 @@ int main(int argc, char* argv[])
   const double inputValue = std::stod(argv[1]);
 
   // TODO 12: Use mysqrt if USE_MYMATH is defined and sqrt otherwise
-
+#ifdef USE_MYMATH
+  const double outputValue = mysqrt(inputValue);
+#else
+  const double outputValue = sqrt(inputValue);
+#endif
   // TODO 6: Replace sqrt with mysqrt
 
   // calculate square root

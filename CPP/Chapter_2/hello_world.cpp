e75e51789c0685730be089178714956f533cdba7
SlaViK2006 <slava.ignatovich@gmail.com>
2025-04-18 15:43:51 +0200

    Complete Chapter 2

---
+++ CPP/Chapter_2/hello_world.cpp
@@ -0,0 +1,9 @@
+// This program outputs the messange "Hello, World!" to the monitor
+
+#include "std_lib_facilities.h"
+
+int main() // C++ programs start by executing the function main
+{
+  cout<<"Hello, World!\n";  // output "hello, World!"
+  return 0;
+}

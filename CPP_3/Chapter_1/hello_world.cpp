1801d3b3093b1a980b01eca6a809fd60ac485f4d
SlaViK2006 <slava.ignatovich@gmail.com>
2025-06-08 18:04:19 +0200

    Trying new feature

---
+++ CPP_3/Chapter_1/hello_world.cpp
@@ -0,0 +1,11 @@
+// This program outputs the message "Hello, World!" to the monitor
+
+import std; //gain access to the C++ standard library
+using namespace std;
+
+int main()
+{
+  std::cout<<"Hello, World!\n";
+
+  return 0;
+}

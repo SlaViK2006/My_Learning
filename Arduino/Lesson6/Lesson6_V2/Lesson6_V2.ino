1b08c947050bba885317ed8495f2b8747d98c031
SlaViK2006 <slava.ignatovich@gmail.com>
2025-08-21 18:58:51 +0200

    The second part of the sixth lesson is finished

--- Arduino/Lesson6/Lesson6_V2/Lesson6_V2.ino
+++ Arduino/Lesson6/Lesson6_V2/Lesson6_V2.ino
@@ -6,11 +6,16 @@ int servoAngle = 0;    //variable to set the angle for the servou motor
 
 void setup() {
   myServo.attach(9);   //attaches the servo on pin 9 to the myServo object
+  Serial.begin(9600);  //open a serial connection to the computer
 }
 
 void loop() {
   potValue = analogRead(potPin);    //store the potentiometer value in a variable 
   servoAngle = map(potValue, 0, 1023, 0, 179);  //scale potentiometer value to the servo
   myServo.write(servoAngle);    //turn the servo to the angle variable
+  Serial.print("potValue: ");   //print a label for the potValue variable
+  Serial.print(potValue);       //print the potValue variable
+  Serial.print(" , servoAngle: ");    //print a label for the servoAngle variable
+  Serial.println(servoAngle);   //print the servoAngle variable
   delay(100);     //wait for the servo to get there
 }

6c1aca345394bdda436a254e4b21ca42ba49bab9
Viachaslau Ihnatovich <slava.ignatovich@gmail.com>
2026-07-04 21:21:53 +0200

    I finished fourth drill of Chapter 3

--- CPP_2/Chapter_3/Drill_1.cpp
+++ CPP_2/Chapter_3/Drill_1.cpp
@@ -12,6 +12,13 @@ int main()
 	string friend_name;
 	cin>>friend_name;       //read characters into friend_name
 	cout<<"Have you seen "<<friend_name<< " lately?\n";
+        cout<<"what is a sex of your friend? Please enter 'm' or 'f':\n";
+	char friend_sex = '0';
+	cin>>friend_sex;
+	if (friend_sex == 'm')
+	  cout<<"If you see "<<friend_name<<" please ask him to call me.\n";
+	else
+          cout<<"If you see "<<friend_name<<" please ask her to call me.\n";
 	
 	return 0;
 }
